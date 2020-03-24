1713505cc77bccdd2f069cd1e2c1cf159009358f
conni <cbuschka@gmail.com>
2020-03-24 08:02:37 +0100

    Added readme.md, license.txt, requirements-dev.txt, setup.py.

---
+++ readme.md
@@ -0,0 +1,11 @@
+# [togglz](https://github.com/cbuschka/python-togglz)
+
+### A feature toggle library designed after Java togglz
+
+## Status
+*Work in Progress*
+
+## License
+Copyright (c) 2020 by [Cornelius Buschka](https://github.com/cbuschka).
+
+[Apache License, Version 2.0](./license.txt)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytogglz",
+    version="0.0.1",
+    author="Cornelius Buschka",
+    author_email="cbuschka@gmail.com",
+    description="A feature toggle library designed after Java togglz",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cbuschka/python-togglz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

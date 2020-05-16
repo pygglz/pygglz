fec3896d886ca322cbfd1d2d261045dd8deb4873
conni <cbuschka@gmail.com>
2020-05-16 16:28:28 +0200

    Moved from github.com/cbuschka to github.com/pygglz.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A feature toggle library designed after Java togglz",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cbuschka/pygglz",
+    url="https://github.com/pygglz/pygglz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",

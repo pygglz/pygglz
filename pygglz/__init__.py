5d190c16de75bfc54585f0e410d8cbbf4946c8a8
conni <cbuschka@gmail.com>
2020-05-17 12:33:07 +0200

    Added docstrings to pygglz module.

--- pygglz/__init__.py
+++ pygglz/__init__.py
@@ -1,8 +1,22 @@
+"""pygglz managed features toggles and is designed after the Java
+togglz framework.
+"""
 from .dummy_repository import DummyRepository
 from .features import Features
 from .file_repository import FileRepository
 from .http_repository import HttpRepository
 
+"""features This is the global access point to feature state.
+
+Use features to query the feature states. features then delegates
+to the current feature context. This can be the global feature
+context or a sub context associated with the current thread, e.g.
+in unit testing contexts.
+"""
 features = Features()
+
+"""configure Main configuration entry point."""
 configure = features.configure
+
+"""new_feature_context Establish a new sub feature context."""
 new_feature_context = features.new_feature_context

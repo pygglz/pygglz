ccc13d33d31a05fe36c0fd514d3dd324f17b9d37
conni <cbuschka@gmail.com>
2020-05-18 07:30:25 +0200

    Added __eq__ and __repr__ to feature state.

--- pygglz/feature_state.py
+++ pygglz/feature_state.py
@@ -2,3 +2,12 @@ class FeatureState(object):
     def __init__(self, name: str, enabled: bool = False):
         self.name = name
         self.enabled = enabled
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __repr__(self):
+        return "<FeatureState name={} enabled={}>".format(self.name, self.enabled)
--- tests/feature_state_test.py
+++ tests/feature_state_test.py
@@ -3,9 +3,25 @@ import unittest
 from pygglz.feature_state import FeatureState
 
 
-class FeatureTest(unittest.TestCase):
+class FeatureStateTest(unittest.TestCase):
     def test_init(self):
         f = FeatureState("Enabled", True)
 
         self.assertEqual("Enabled", f.name)
         self.assertTrue(f.enabled)
+
+    def test_eq_if_attributes_match(self):
+        a_enabled = FeatureState("a", enabled=True)
+        self.assertEqual(FeatureState("a", enabled=True), a_enabled)
+
+    def test_ne_if_name_doesnt_match(self):
+        a = FeatureState("a", enabled=True)
+        self.assertNotEqual(FeatureState("b", enabled=True), a)
+
+    def test_ne_if_enabled_doesnt_match(self):
+        a = FeatureState("a", enabled=True)
+        self.assertNotEqual(FeatureState("a", enabled=False), a)
+
+    def test_repr(self):
+        a = FeatureState("a", enabled=True)
+        self.assertEqual("<FeatureState name=a enabled=True>", repr(a))
